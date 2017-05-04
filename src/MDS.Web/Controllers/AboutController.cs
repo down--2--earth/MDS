ab2740b3c7b2d9ca476d846c745147ab0315d636
mahesh.ksl@gmail.com <mahesh.ksl@gmail.com>
2017-05-04 18:50:27 +0530

    Added Attribute routing

--- src/MDS.Web/Controllers/AboutController.cs
+++ src/MDS.Web/Controllers/AboutController.cs
@@ -8,14 +8,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MDS.Web.Controllers
 {
+    //[Route("About")]
+    //[Route("abc/[controller]/[action]")]
+    [Route("abc/[controller]")]
     public class AboutController
     {
         // GET: /<controller>/
+        [Route("")]
         public string Phone()
         {
             return "1-555-555-555";
         }
 
+        [Route("country")]
+        //[Route("action")]
         public string Country()
         {
             return "SL";
